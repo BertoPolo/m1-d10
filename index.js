eca04bc8401e733bda67572d1288f91a78df558a
BertoPolo <bertopolo91@gmail.com>
2021-12-10 12:23:30 +0100

    hehe

---
+++ index.js
@@ -0,0 +1,20 @@
+let x = "John"
+let y ="Doe"
+console.log(x +" <> "+ y)
+
+const bigGuy ={
+    name: "Alejandro",
+    surname: "Perrete",
+    email: "alePerro@hotmail.ru",
+}
+console.log(bigGuy)
+
+delete bigGuy.email
+
+console.log(bigGuy)
+
+const arry = ["hi","wassup","vamonoh","five","six","buenoooo","almost","finishin","aleluya"]
+
+
+
+
