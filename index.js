8da53af42241f68c1eeb3de89df8a987ea0fe003
BertoPolo <bertopolo91@gmail.com>
2021-12-10 13:25:03 +0100

    asd

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let x = "John"
 let y ="Doe"
 console.log(x +" <> "+ y)
 
+
+
+
 const bigGuy ={
     name: "Alejandro",
     surname: "Perrete",
@@ -9,13 +12,21 @@ const bigGuy ={
 }
 console.log(bigGuy)
 
+
+
+
 delete bigGuy.email
 
 console.log(bigGuy)
 
+
+
+
 const arry = ["hi","wassup","vamonoh","five","six","buenoooo","almost","finishin","aleluya"]
 
 console.log(arry)
+
+
 
 let bigArry=[]
 
@@ -26,4 +37,36 @@ for(let i=0;i<100;i++){
 }
 console.log(bigArry)
 
+// you need to use [i], so every generate number willl insert in the bigArray.let arrayRandom26=[]
+// for(i=0; i<100; i++){
+//     arrayRandom26[i]=Math.floor(Math.random()*100)+1
+// }
 
+
+
+
+let mainArray=[subArray1,subArray2]
+let subArray1=[]
+let subArray2=[]
+
+
+
+
+// 29
+
+const lengthSeeker = function(arr1,arr2){
+    arr1.length() < arr2.length() ? arr2 : arr1
+}
+
+
+//31 
+
+const getter = function(){
+    let getterNode = document.getElementById("container")
+}
+
+//32
+
+const gettertwo = function(){
+    let getterNode = document.getElementsByTagName("td")
+}
